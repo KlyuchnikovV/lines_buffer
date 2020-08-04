c42712ebbc317caeefad039aa9962da79a50570e
KlyuchnikovV <valeriy.v.klyuchnikov@gmail.com>
2020-08-04 11:10:22 +0300

    Api extension

--- buffer.go
+++ buffer.go
@@ -46,27 +46,27 @@ func (b *Buffer) DeleteBackward() {
 		}
 
 		b.row--
-		b.column = len(b.currentRow())
+		b.column = len(b.CurrentRow())
 		b.mergeLineWithNext()
 	} else {
-		b.lines[b.row] = string(append([]byte(b.currentRow())[:b.column-1], []byte(b.currentRow())[b.column:]...))
+		b.lines[b.row] = string(append([]byte(b.CurrentRow())[:b.column-1], []byte(b.CurrentRow())[b.column:]...))
 		b.column--
 	}
 }
 
 func (b *Buffer) DeleteForward() {
-	if b.column == len(b.currentRow()) {
+	if b.column == len(b.CurrentRow()) {
 		if b.row == len(b.lines)-1 {
 			return
 		}
 		b.mergeLineWithNext()
 	} else {
-		b.lines[b.row] = string(append([]byte(b.currentRow())[:b.column], []byte(b.currentRow())[b.column+1:]...))
+		b.lines[b.row] = string(append([]byte(b.CurrentRow())[:b.column], []byte(b.CurrentRow())[b.column+1:]...))
 	}
 }
 
 func (b *Buffer) mergeLineWithNext() {
-	b.lines[b.row] = b.currentRow() + b.lines[b.row+1]
+	b.lines[b.row] = b.CurrentRow() + b.lines[b.row+1]
 	b.lines = append(b.lines[:b.row+1], b.lines[b.row+2:]...)
 }
 
@@ -85,6 +85,80 @@ func (b *Buffer) String() string {
 	return strings.Join(b.lines, "\n")
 }
 
-func (b *Buffer) currentRow() string {
+func (b *Buffer) CurrentRow() string {
 	return b.lines[b.row]
+}
+
+func (b *Buffer) Lines() []string {
+	return b.lines
+}
+
+func (b *Buffer) RowNum() int {
+	return b.row
+}
+
+func (b *Buffer) ColumnNum() int {
+	return b.column
+}
+
+func (b *Buffer) PrevLine() {
+	if b.row == 0 {
+		return
+	}
+	b.row--
+	if len(b.CurrentRow()) < b.column {
+		b.column = len(b.CurrentRow())
+	}
+}
+
+func (b *Buffer) NextLine() {
+	if b.row == len(b.lines)-1 {
+		return
+	}
+	b.row++
+	if len(b.CurrentRow()) < b.column {
+		b.column = len(b.CurrentRow())
+	}
+}
+
+func (b *Buffer) NextRune() {
+	if b.column == len(b.CurrentRow()) {
+		b.NewLine()
+		b.column = 0
+	} else {
+		b.column++
+	}
+}
+
+func (b *Buffer) PrevRune() {
+	if b.column == 0 {
+		b.PrevLine()
+		b.column = len(b.CurrentRow())
+	} else {
+		b.column--
+	}
+}
+
+func (b *Buffer) MoveForward(n int) {
+	for i := 0; i < n; i++ {
+		b.NextRune()
+	}
+}
+
+func (b *Buffer) MoveBackward(n int) {
+	for i := 0; i < n; i++ {
+		b.PrevRune()
+	}
+}
+
+func (b *Buffer) MoveDown(n int) {
+	for i := 0; i < n; i++ {
+		b.NextLine()
+	}
+}
+
+func (b *Buffer) MoveUp(n int) {
+	for i := 0; i < n; i++ {
+		b.PrevLine()
+	}
 }
